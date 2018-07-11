e261de8c22b7b44929b22f71fde10e2ad02b90d3
junkapi <numagamijn@gmail.com>
2018-07-11 08:11:20 +0800

    if機能とインジェクション、現在時刻表示機能追加

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,7 +1,46 @@
 <?php
-  // ここにDBに登録する処理を記述する
 
+
+
+  // データベースに接続する
+  $dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+  $user ='root';
+  $passward='';
+  $dbh = new PDO($dsn, $user, $passward);
+  $dbh->query('SET NAMES utf8');
+
+  // POSTでデータが送信された時のみSQLを実行する
+  if (!empty($_POST)) {
+
+
+          // ここにDBに登録する処理を記述する
+      $nickname = htmlspecialchars($_POST['nickname']);
+      $comment = htmlspecialchars($_POST['comment']);
+
+      if (!empty($nickname || $comment)) {
+
+
+
+      // SOL文を実行する
+      $sql = 'INSERT INTO `posts`(`nickname`, `comment`) VALUES (?,?)';
+
+      // インジェクション
+
+      $data[] = $nickname;
+      $data[] = $comment;
+
+
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute($data);
+  
+        }
+
+  }
+
+  // データベースを切断する
+  $dbh = null
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -12,6 +51,10 @@
     <form method="post" action="">
       <p><input type="text" name="nickname" placeholder="nickname"></p>
       <p><textarea type="text" name="comment" placeholder="comment"></textarea></p>
+      <?php  
+       date_default_timezone_set('Asia/Manila');
+       echo '<p>', date('Y年m月d日 H時i分s秒'), 'Asia/Manila', '<p>';
+      ?>
       <p><button type="submit" >つぶやく</button></p>
     </form>
     <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
